09be0662e35aec8b27e6cc202edf95dc38ea937a
aleksejunas <photo@seventyseven.no>
2025-03-27 23:45:20 +0100

    removed som e unused imports

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,19 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import ThemeSwitcher from "./components/ThemeSwitcher";
-// import Navbar from "./components/navbar/Navbar";
 // import { BrowserRouter as Router } from "react-router-dom";
 import "typeface-roboto";
-
-// FIXME: Get the global navbar working
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <Router> */}
     <App />
-    {/* <Navbar /> */}
-    <ThemeSwitcher />
     {/* </Router> */}
   </StrictMode>,
 );
